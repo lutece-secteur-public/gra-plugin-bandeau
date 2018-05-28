797d8cbfa31cfae7fa2a7c8f4bef7bfaf2d42000
AlexandreClose <alexandre.close@gmail.com>
2018-05-28 14:45:46 +0200

    PLUGBAND-2 - Prevent back_url from XSS injections and OpenRedirect : allow specifics domains and JS escape back_url in bandeau template with freemarker

--- src/java/fr/paris/lutece/plugins/bandeaugra/web/BandeaugraApp.java
+++ src/java/fr/paris/lutece/plugins/bandeaugra/web/BandeaugraApp.java
@@ -40,13 +40,16 @@ import javax.servlet.http.HttpServletRequest;
 import fr.paris.lutece.portal.service.security.LuteceUser;
 import fr.paris.lutece.portal.service.security.SecurityService;
 import fr.paris.lutece.portal.service.security.UserNotSignedException;
+import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.service.util.AppPathService;
+import fr.paris.lutece.portal.service.util.AppPropertiesService;
 import fr.paris.lutece.portal.util.mvc.commons.annotations.View;
 import fr.paris.lutece.portal.util.mvc.xpage.MVCApplication;
 import fr.paris.lutece.portal.util.mvc.xpage.annotations.Controller;
 import fr.paris.lutece.portal.web.constants.Markers;
 import fr.paris.lutece.portal.web.xpages.XPage;
-
+import java.net.MalformedURLException;
+import java.net.URL;
 /**
  * This class provides a simple implementation of an XPage
  */
@@ -57,6 +60,8 @@ public class BandeaugraApp extends MVCApplication
     private static final String VIEW_AUTH = "auth";
     private static final String PARAMETER_BACK_URL = "back_url";
     private static final String MARK_BACK_URL = "back_url";
+    private static final String PROPERTY_AUTHORIZED_DOMAINS = "bandeaugra.authorizedDomains";
+    private static final String CONSTANT_COMMA = ",";
 
     /**
      * Returns the content of the page bandeaugra.
@@ -80,7 +85,11 @@ public class BandeaugraApp extends MVCApplication
 
         Map<String, Object> model = getModel( );
         model.put( Markers.BASE_URL, AppPathService.getBaseUrl( request ) );
-        model.put( MARK_BACK_URL, strBackUrl );
+        
+        if ( strBackUrl != null )
+        {
+            model.put( MARK_BACK_URL, strBackUrl );
+        }
 
         XPage xpage = getXPage( TEMPLATE_XPAGE, request.getLocale( ), model );
         xpage.setStandalone( true );
@@ -90,6 +99,24 @@ public class BandeaugraApp extends MVCApplication
 
     private static String getBackUrl( HttpServletRequest request )
     {
-        return request.getParameter( PARAMETER_BACK_URL );
+        String strBackUrl = request.getParameter( PARAMETER_BACK_URL );
+        String[] listAuthorizedDomains = AppPropertiesService.getProperty( PROPERTY_AUTHORIZED_DOMAINS ).split( CONSTANT_COMMA );
+
+        try 
+        {
+            URL url = new URL ( strBackUrl );
+            String strHost = url.getHost( );
+            for ( String strAuthorizedDomain : listAuthorizedDomains )
+            {
+                if ( strHost.endsWith( strAuthorizedDomain ) )
+                {
+                    return strBackUrl;
+                }
+            }
+        } catch ( MalformedURLException ex) 
+        {
+            AppLogService.error( "Given back_url isn't a valid url " + strBackUrl, ex);
+        }
+        return null;
     }
 }
